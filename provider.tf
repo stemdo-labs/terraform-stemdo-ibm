e84d8bfba160b4cece829bd4807a630c0ebb8f26
ploopez <plopez@stemdo.io>
2024-11-06 12:52:56 +0100

    update

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
-variable "ibmcloud_api_key" {}
+//variable "ibmcloud_api_key" {}
 
 provider "ibm" {
   ibmcloud_api_key   = var.ibmcloud_api_key
@@ -6,7 +6,7 @@ provider "ibm" {
   region             = "us-south"
 }
 
-output "debug_ibmcloud_api_key" {
-  value     = var.ibmcloud_api_key
-  sensitive = false  # Caution: not recommended for secrets
-}
+# output "debug_ibmcloud_api_key" {
+#   value     = var.ibmcloud_api_key
+#   sensitive = false  # Caution: not recommended for secrets
+# }
