3dfc9e6aac6209ae0a26eefcf564b388963f6717
davidnboffelli <166407728+davidnboffelli@users.noreply.github.com>
2024-11-25 10:06:01 +0100

    change vpc name

--- vpc.tf
+++ vpc.tf
@@ -3,13 +3,13 @@ data "ibm_resource_group" "resource_group" {
 }
 
 resource "ibm_is_vpc" "vpc" {
-  name = "StemdoSandboxvpc"
+  name = "stemdosandboxvpc"
   # resource_group = "4364ced224cf420fa07d8bf70a8d70df"
   resource_group = data.ibm_resource_group.resource_group.id
 }
 
 resource "ibm_is_subnet" "subnet" {
-  name              = "Stemdo_Sandbox_subnet-1"
+  name              = "stemdosandboxsubnet-1"
   vpc               = ibm_is_vpc.vpc.id
   zone              = "eu-es-2"
   ipv4_cidr_block   = "10.10.0.0/24"  # Cambia el rango de IP si es necesario
