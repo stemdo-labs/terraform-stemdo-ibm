53ade75e3fe7d1f4031e5afb0333c692c151f4e5
davidnboffelli <166407728+davidnboffelli@users.noreply.github.com>
2024-11-25 12:07:32 +0100

    routing

--- vpc.tf
+++ vpc.tf
@@ -8,16 +8,23 @@ resource "ibm_is_vpc" "vpc" {
   resource_group = data.ibm_resource_group.resource_group.id
 }
 
-resource "ibm_is_vpc_routing_table" "example" {
-  name = "example-routing-table"
-  vpc  =  ibm_is_vpc.vpc.id
+resource "ibm_is_vpc_address_prefix" "example" {
+  cidr = "10.0.1.0/24"
+  name = "example-add-prefix"
+  vpc  = ibm_is_vpc.vpc.id
+  zone = "eu-es-1"
 }
 
 resource "ibm_is_subnet" "subnet" {
+
+  depends_on = [
+    ibm_is_vpc_address_prefix.example
+  ]
+
   name              = "stemdosandboxsubnet-1"
   vpc               = ibm_is_vpc.vpc.id
   zone              = "eu-es-1"
-  ipv4_cidr_block   = "10.240.0.0/24"  # Cambia el rango de IP si es necesario
+  ipv4_cidr_block = "10.0.1.0/24"
   resource_group = data.ibm_resource_group.resource_group.id
   routing_table   = ibm_is_vpc_routing_table.example.routing_table
 }
