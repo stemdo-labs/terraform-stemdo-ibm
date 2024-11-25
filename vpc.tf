e17fa14e0ca3df3e61bbf5acf05dc940f4dc46e5
davidnboffelli <166407728+davidnboffelli@users.noreply.github.com>
2024-11-25 11:30:11 +0100

    ipv4_cidr_block or total_ipv4_address_count need to be provided

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,7 @@ resource "ibm_is_subnet" "subnet" {
   name              = "stemdosandboxsubnet-1"
   vpc               = ibm_is_vpc.vpc.id
   zone              = "eu-es-1"
-  # ipv4_cidr_block   = "10.10.0.0/24"  # Cambia el rango de IP si es necesario
+  ipv4_cidr_block   = "10.10.0.0/24"  # Cambia el rango de IP si es necesario
   # resource_group    = "4364ced224cf420fa07d8bf70a8d70df"
   resource_group = data.ibm_resource_group.resource_group.id
 }
