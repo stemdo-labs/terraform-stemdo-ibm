7d2000af34ffc10944f5509187d1fa4543a6e7e2
davidnboffelli <166407728+davidnboffelli@users.noreply.github.com>
2024-11-25 16:01:52 +0100

    Update openshift.tf

--- openshift.tf
+++ openshift.tf
@@ -11,7 +11,7 @@ resource "ibm_container_vpc_cluster" "example_openshift" {
   kube_version        = "4.16.19_openshift"                         # Especifica la versión de OpenShift
   # hardware          = "shared"                                    # Usar nodos dedicados (dedicated) o compartidos (shared)
   # worker_pool_name  = "default"
-  worker_count        = "2"                                         # Número de nodos trabajadores
+  worker_count        = "3"                                         # Número de nodos trabajadores
   flavor              = "bx2.4x16"                                  # Tipo de máquina de los nodos
   # zone              = "eu-es-2"                                   # Zona donde se creará el clúster
   vpc_id              = ibm_is_vpc.vpc.id                           # ID de la VPC
