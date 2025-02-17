e57d28ab72347e339e741f16b8ee57c469affb34
abermudez-14 <abermudez14@stemdo.io>
2025-02-17 12:03:55 +0100

    Wiki Main

--- Wiki/main.tf
+++ Wiki/main.tf
@@ -17,13 +17,25 @@ resource "ibm_iam_access_group" "Stemdo_TEST" {
   description = "Resource group for IAM access for Wiki users"
 }
 
+
+# resource "ibm_iam_access_group_policy" "TestPolicy" {
+#   access_group_id = "AccessGroupId-3c278dfb-5193-4ffc-95b3-e796f46a8187"
+#   roles           = ["Editor","Viewer","Manager","Operator"]
+  
+
+#   resources {
+#     service = "container-registry"
+#     resource_group_id = var.resource_group
+#   }
+# }
+
 resource "ibm_iam_access_group_policy" "KubernetesPolicy" {
   access_group_id = ibm_iam_access_group.Stemdo_TEST.id
   roles           = ["Editor","Viewer","Manager","Operator"]
   
 
   resources {
-    service = "cloud-object-storage"
+    service = "container-registry"
     resource_group_id = var.resource_group
   }
 }
